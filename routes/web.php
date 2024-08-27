e975e3853d2156b8c10b11b1359dc2e60b9d5ae5
J_a_l_i_s <124979469+Jalismahamud@users.noreply.github.com>
2024-08-27 15:08:03 +0600

    "Added SSLCOMMERZ payment routes and exempted related URLs from CSRF token verification"

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Middleware\TokenVerificationMiddleware;
-
+use App\Http\Controllers\SslCommerzPaymentController;
 
 
 
@@ -87,3 +87,19 @@ Route::get('/summary',[DashboardController::class,'Summary'])->middleware([Token
 //Report page Route
 Route::get('/reportPage',[ReportController::class,'ReportPage'])->middleware([TokenVerificationMiddleware::class]);
 Route::get('/sales-report/{FormDate}/{ToDate}',[ReportController::class,'SalesReport'])->middleware([TokenVerificationMiddleware::class]);
+
+
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
